e70f350fce76e43c3bcd06edcee6126b6252bcc6
Wilson Siaw <siawwils90@gmail.com>
2023-02-22 10:54:27 -0800

    Fix the 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
  * print string - Print string using puts()
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
-	return(0);
+	return (0);
 }
