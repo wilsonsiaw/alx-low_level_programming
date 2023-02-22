4e541fe5572fec20dbdf99cba984a2432b7a1d62
Wilson Siaw <siawwils90@gmail.com>
2023-02-22 11:08:14 -0800

    Add the 5-printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A function that prints a string
+ * using the printf() function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
