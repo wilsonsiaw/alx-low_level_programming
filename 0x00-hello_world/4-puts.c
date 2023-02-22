55a11fdc2d1b6ac315974872e80cbb90b01729f2
Wilson Siaw <siawwils90@gmail.com>
2023-02-22 10:36:38 -0800

    Add the 4-puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print string - Print string using puts()
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return(0);
+}
